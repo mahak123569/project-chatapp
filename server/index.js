1f24ee808ebf6007eb1fa93847866f7a6b17aa65
mahak <kaushikikes@gmail.com>
2026-07-09 21:09:05 +0530

    book

--- server/index.js
+++ server/index.js
@@ -1,2 +1,8 @@
-const express = require("express");
-const cors = require("cors"); 
+import express from "express";
+import {PORT} from "./config.js";
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

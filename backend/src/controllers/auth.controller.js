7f5d893d7cdc3d56136efffc30c88e52aa493fbb
mahak <kaushikikes@gmail.com>
2026-07-19 22:50:18 +0530

    update

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -124,3 +124,11 @@ console.log("Error in update profile:",error);
 res.status(500).json({message: "Internal Server Error"});
    }
 };
+ export const checkAuth = (req,res) => {
+    try{
+        res.status(200).json(req.user);
+    } catch (error){
+        console.log("Error in checkAuth controller",error.message);
+    res.status(500).json({message: "Internal Server Error"});
+    }
+ }

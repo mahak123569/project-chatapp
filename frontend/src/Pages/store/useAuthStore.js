dfb93627e5cd26ae396bb50eb32978f5b50733d0
mahak <kaushikikes@gmail.com>
2026-07-25 23:02:18 +0530

    auth

--- frontend/src/Pages/store/useAuthStore.js
+++ frontend/src/Pages/store/useAuthStore.js
@@ -1,5 +1,21 @@
 import { create } from "zustand";
+import { axiosInstanace } from "../../components/lib/axios";
 export const useAuthStore = create((set)=>({
     authUser : null,
+    isSigningUp: false,
+    isLoggingIng:false,
+    isUpdatingProfile:false,
     isCheckingAuth:true,
+
+    isCheckingAuth:true,
+    checkAuth: async() => {
+        try {
+            const res = await axiosInstanace.get("/auth/check")
+            set({authUser:res.data})
+        } catch (error){
+            set({authUser:null});
+        } finally {
+            set({isCheckingAuth:false});
+        }
+    },
 }));

40d058025e7a37a56cc371c16a7de0cd5593342a
mahak <kaushikikes@gmail.com>
2026-08-03 22:39:46 +0530

    message

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,9 @@
+const Sidebar = () => {
+  return (
+    <div className="w-72 border-r border-base-300 p-4">
+      Sidebar
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ frontend/src/components/chatcontainer.jsx
@@ -0,0 +1,9 @@
+const ChatContainer = () => {
+  return (
+    <div className="flex-1 flex items-center justify-center">
+      Chat Container
+    </div>
+  );
+};
+
+export default ChatContainer;

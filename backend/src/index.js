75602de85a9aea75a2e6d65268d2a7cf2db4edde
mahak <kaushikikes@gmail.com>
2026-08-06 16:35:34 +0530

    update index

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,17 @@
-import express from 'express';
-import dotenv from "dotenv"
-import cookieParser from "cookie-parser"
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 
-import { connectDB } from './lib/db.js';
-
-import authRoutes from './routes/auth.route.js';
-import messageRoutes from './routes/message.route.js';
-
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
-const app = express();
 
-const PORT = process.env.PORT
+const app = express();
+const PORT = process.env.PORT;
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -21,14 +20,12 @@ app.use(
     origin: "http://localhost:5173",
     credentials: true,
   })
-
 );
 
-
-app.use("/api/auth",authRoutes);
-app.use("/api/message",messageRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
-  console.log("Server is running on PORT:"+ PORT);
-  connectDB()
+  console.log("Server is running on PORT:", PORT);
+  connectDB();
 });
